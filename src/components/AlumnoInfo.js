ad8f03701207d16b84104ed66d657960e5172ab8
WalterRS <15030122@itcelaya.edu.mx>
2020-06-12 00:34:42 -0500

    Prueba

--- src/components/AlumnoInfo.js
+++ src/components/AlumnoInfo.js
@@ -1,7 +1,31 @@
-import React from 'react';
-import './AlumnoInfo.css';
-class AlumnoInfo {
-//Nombre del alumno
-//Gera es puto
-//ok
+import React, {Component} from 'react';
+import { img } from 'react-bootstrap';
+
+class AlumnoInfo extends Component{
+    render() {
+        return (
+            <div className="Container">
+                <div className="foto_alumno" >
+                    <img src="..." alt="..." class="img-circle"/>
+               </div>
+                <div className="nombre_alumno">
+                    <h2>Nombre</h2>
+                </div>
+                <div className="apellidos_alumno">
+                    <h2>Apellidos</h2>
+                </div>
+                <div className="">
+                    <h2>Estado</h2>
+                </div>
+                <div>
+                    <h2>email</h2>
+                </div>
+                <div>
+                    <h2>Edad</h2>
+                </div>
+            </div>
+        );
+    }
 }
+export default AlumnoInfo;
+    
